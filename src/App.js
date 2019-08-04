177a7d384901ebbb31fb5d59b46b430eef524b5e
eduardo <eduloni3@gmail.com>
2019-08-04 16:18:38 +0100

    react router test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import {
+  About,
+  Cart,
+  FAQ,
+  Gallery,
+  Home,
+  Nav,
+  Shop,
+  ShopItem,
+} from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/shop/:id" component={ShopItem} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/FAQ" component={FAQ} />
+          <Route path="/about" component={About} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
