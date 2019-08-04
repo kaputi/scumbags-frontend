33f87cd6a2971b85fa4070a57b99e2be11b6e4ea
eduardo <eduloni3@gmail.com>
2019-08-04 16:16:49 +0100

    component index

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import About from './About'
+import Cart from './Cart'
+import FAQ from './FAQ'
+import Gallery from './Gallery'
+import Home from './Home'
+import Nav from './Nav'
+import Shop from './Shop'
+import ShopItem from './ShopItem'
+
+export { About, Cart, FAQ, Gallery, Home, Nav, Shop, ShopItem }
