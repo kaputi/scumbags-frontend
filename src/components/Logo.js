5071e7ff4f0a8fd151665692e98f6e060bc0dc4e
eduardo <eduloni3@gmail.com>
2019-08-08 10:20:35 +0100

    logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,21 +1,25 @@
 import styled from 'styled-components'
 
-const Logo = styled.img.attrs({
-  logoSize: (props) => {
-    const { size } = props
-    const { small, medium, large } = props.theme.images.sizes
-    switch (size) {
-      case 'small':
-        return small
-      case 'medium':
-        return medium
-      case 'large':
-        return large
-      default:
-        return size
-    }
-  },
-  src: ({ theme }) => theme.images.logo,
+const Logo = styled.img.attrs((props) => {
+  const { size, theme } = props
+  const { small, medium, large } = props.theme.images.sizes
+  let logoSize = size
+  switch (size) {
+    case 'small':
+      logoSize = small
+      break
+    case 'medium':
+      logoSize = medium
+      break
+    case 'large':
+      logoSize = large
+      break
+    default:
+      logoSize = size
+      break
+  }
+
+  return { src: theme.images.logo, logoSize }
 })`
   width: ${({ logoSize }) => logoSize};
 `
