96b741fa2cc3c5648ee9e04cbc991cddc49804ea
eduardo <eduloni3@gmail.com>
2019-08-09 16:19:23 +0100

    social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import SocialLinks from './SocialLinks'
 
 import FooterNav from './FooterNav'
 
@@ -7,10 +8,13 @@ const CopyRight = styled.p`
   color: ${({ theme }) => theme.menuItemColor};
 `
 
-const FooterWrapper = styled.div``
+const FooterWrapper = styled.div`
+  padding-top: 50px;
+`
 
 const Footer = () => (
   <FooterWrapper>
+    <SocialLinks />
     <FooterNav />
     <CopyRight>© Scumbags Messenger Bags 2019</CopyRight>
   </FooterWrapper>
