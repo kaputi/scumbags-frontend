f483b9e0d463502355c2b1260f392b3c80100c5d
eduardo <eduloni3@gmail.com>
2019-08-09 16:17:36 +0100

    font awesome library

---
+++ src/config/fontAwesome.js
@@ -0,0 +1,10 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// import your icons
+import {
+  faInstagram,
+  faFacebookSquare,
+} from '@fortawesome/free-brands-svg-icons'
+
+library.add(faInstagram, faFacebookSquare)
